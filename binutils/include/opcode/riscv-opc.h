1b8cc26317fdd0d4d6341469de0490392d4a93cb
Eric Flamand <eric.flamand@greenwaves-technologies.com>
2017-02-02 18:13:49 +0100

    fix csr misa encoding 1.9

--- binutils/include/opcode/riscv-opc.h
+++ binutils/include/opcode/riscv-opc.h
@@ -862,10 +862,10 @@
 #define	CSR_UEPC	0x041
 #define	CSR_UCAUSE	0x042
 #define	CSR_UBADADDR	0x043
-#define	CSR_UIP	0x044
+#define	CSR_UIP		0x044
 
 #define	CSR_FFLAGS	0x001
-#define	CSR_FRM	0x002
+#define	CSR_FRM		0x002
 #define	CSR_FCSR	0x003
 
 #define	CSR_CYCLE	0xc00
@@ -878,14 +878,14 @@
 #define	CSR_SSTATUS	0x100
 #define	CSR_SEDELEG	0x102
 #define	CSR_SIDELEG	0x103
-#define	CSR_SIE	0x104
+#define	CSR_SIE		0x104
 #define	CSR_STVEC	0x105
 
 #define	CSR_SSCRATCH	0x140
 #define	CSR_SEPC	0x141
 #define	CSR_SCAUSE	0x142
 #define	CSR_SBADADDR	0x143
-#define	CSR_SIP	0x144
+#define	CSR_SIP		0x144
 
 #define	CSR_SPTBR	0x180
 
@@ -899,7 +899,7 @@
 #define	CSR_HSTATUS	0x200
 #define	CSR_HEDELEG	0x202
 #define	CSR_HIDELEG	0x203
-#define	CSR_HIE	0x204
+#define	CSR_HIE		0x204
 #define	CSR_HTVEC	0x205
 
 #define	CSR_HSCRATCH	0x240
@@ -914,23 +914,23 @@
 #define	CSR_HTIMEH	0xe81
 #define	CSR_HINSTRETH	0xe82
 
-#define	CSR_MISA	0xf10
 #define	CSR_MVENDORID	0xf11
 #define	CSR_MARCHID	0xf12
 #define	CSR_MIMPID	0xf13
 #define	CSR_MHARTID	0xf14
 
 #define	CSR_MSTATUS	0x300
+#define	CSR_MISA	0x301
 #define	CSR_MEDELEG	0x302
 #define	CSR_MIDELEG	0x303
-#define	CSR_MIE	0x304
+#define	CSR_MIE		0x304
 #define	CSR_MTVEC	0x305
 
 #define	CSR_MSCRATCH	0x340
 #define	CSR_MEPC	0x341
 #define	CSR_MCAUSE	0x342
 #define	CSR_MBADADDR	0x343
-#define	CSR_MIP	0x344
+#define	CSR_MIP		0x344
 
 #define	CSR_MBASE	0x380
 #define	CSR_MBOUND	0x381
